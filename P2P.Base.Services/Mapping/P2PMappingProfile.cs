340b9ad63f5efcc0f2d8b63d5102f4a4606bacb5
Nenad Horvat <horvat.nenad02@gmail.com>
2022-10-06 13:32:55 +0200

    *

--- P2P.Base.Services/Mapping/P2PMappingProfile.cs
+++ P2P.Base.Services/Mapping/P2PMappingProfile.cs
@@ -85,12 +85,12 @@ namespace P2P.Base.Services.Mapping
             CreateMap<Review, ReviewODTO>()
                .ForMember(dest => dest.Languagename, source => source.MapFrom(m => m.Language.LanguageName))
                .ForMember(dest => dest.SerpTitle, source => source.MapFrom(m => m.Serp.SerpTitle))
-               .ForMember(dest => dest.FacebookUrl, source => source.MapFrom(m => m.Rev_FacebookUrl.Rev_FacebookUrls))
-               .ForMember(dest => dest.InstagramUrl, source => source.MapFrom(m => m.Rev_InstagramUrl.Rev_InstagramUrls))
-               .ForMember(dest => dest.LinkedInUrl, source => source.MapFrom(m => m.Rev_LinkedInUrl.Rev_LinkedIdUrls))
-               .ForMember(dest => dest.TwitterUrl, source => source.MapFrom(m => m.Rev_TwitterUrl.Rev_TwitterUrls))
-               .ForMember(dest => dest.YoutubeUrl, source => source.MapFrom(m => m.Rev_YoutubeUrl.Rev_YoutubeUrls))
-               .ForMember(dest => dest.ReportLink, source => source.MapFrom(m => m.Rev_ReportLink.Rev_ReportLinks));
+               .ForMember(dest => dest.FacebookUrlName, source => source.MapFrom(m => m.Rev_FacebookUrl.URL))
+               .ForMember(dest => dest.InstagramUrlName, source => source.MapFrom(m => m.Rev_InstagramUrl.URL))
+               .ForMember(dest => dest.LinkedInUrlName, source => source.MapFrom(m => m.Rev_LinkedInUrl.URL))
+               .ForMember(dest => dest.TwitterUrlName, source => source.MapFrom(m => m.Rev_TwitterUrl.URL))
+               .ForMember(dest => dest.YoutubeUrlName, source => source.MapFrom(m => m.Rev_YoutubeUrl.URL))
+               .ForMember(dest => dest.ReportUrlName, source => source.MapFrom(m => m.Rev_ReportLink.URL));
 
             CreateMap<ReviewIDTO, Review>();
 
