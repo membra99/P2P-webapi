f36bc7ce9fb990a7b6d0472ddac0156c948bd82b
Mihajlo Zigic <mihalozigalo@gmail.com>
2022-10-17 17:05:28 +0200

    *

--- P2P.Services/TokenService.cs
+++ P2P.Services/TokenService.cs
@@ -20,7 +20,7 @@ namespace P2P.Services
     }
     public class TokenService : BaseService, ITokenService
     {
-        private const double EXPIRY_DURATION_MINUTES = 30;
+        private const double EXPIRY_DURATION_MINUTES = 240;
         public TokenService(MainContext context, IMapper mapper) : base(context, mapper)
         {
         }
